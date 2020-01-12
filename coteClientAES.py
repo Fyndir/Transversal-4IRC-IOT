3cf41a11cbc63545efe6a74de81fed679445e352
Neexos <leomeynet74@gmail.com>
2020-01-12 15:27:59 +0100

    azure

--- coteClientAES.py
+++ coteClientAES.py
@@ -1,12 +1,11 @@
 # encoding: utf-8
+from azure.iot.device import IoTHubDeviceClient, Message
 import xxtea, requests, serial, time, sys
-#from influxdb_client import InfluxDBClient
 import multiprocessing as mp
-
-
 
 #---------- VARS --------------
 url = "https://emergencymanager.azurewebsites.net/fire/send"
+
 
 SERIALPORT = "/dev/ttyUSB1"
 BAUDRATE = 115200
@@ -57,17 +56,54 @@ def sendHttp(buffer):
         except:
             print("error")
 
+
+#------------- IOT TO AZURE ----------------------------
+MSG_TXT = '{{"id": {id},"intensity": {intensity}}}'
+CONNECTION_STRING = "HostName=Transversal.azure-devices.net;DeviceId=MyPythonDevice;SharedAccessKey=uexoo0Y/uBIF5Rw/WWfedQ7Vjl64G/zStMHP9oSgNaU="
+
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
+
+def iothub_client_telemetry_sample_run(buffer):
+    try:
+        client = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+        while True:
+            # Build the message with simulated telemetry values.
+            data = str(buffer.get())
+            data = data.replace("\'", "")
+            data = data.replace("b", "")
+            tab = data.split(';')
+            tab = tab[:-1]
+            for couple in tab:
+                new = couple.split(',')
+                deviceId = new[0]
+                fireIntensity = new[1]
+                msg_txt_formatted = MSG_TXT.format(id=deviceId, intensity=fireIntensity)
+                message = Message(msg_txt_formatted)
+                # Send the message.
+                print( "Sending message: {}".format(message) )
+                client.send_message(message)
+                print ( "Message successfully sent" )
+    except KeyboardInterrupt:
+        print ( "IoTHubClient sample stopped" )
+        sys.exit()
+
+
+
+
 it = 0
 myStr = ""
-
 def main():
     global myStr, it
     print('debut')
     print('Attente de message')
     encryptedData = bytes(readUARTMessage())
-    print('decryptage du message : '+encryptedData)
+    #print('decryptage du message : '+encryptedData.decode('utf-8'))
     decrypted = xxtea.decrypt(encryptedData, key)
-    print('message decrypte : '+decrypted)
+    #print('message decrypte : '+decrypted)
     finalRet = parseX(str(decrypted))
     print('enleve les x : '+finalRet)
     if(len(finalRet) != 0):
@@ -78,8 +114,9 @@ def main():
                 it += 1
         print('nb données dans buffer :'+str(it))
     if it >= 50:
-        print('envoie dans la queue de P2 : ' + myStr)
+        print('envoie dans la queue de P2 : ' + str(myStr))
         bufferPost.put(myStr)
+        bufferIoT.put(myStr)
         myStr = ""
         it = 0
         #print(finalRet)
@@ -89,9 +126,11 @@ def main():
 #---------- WHILE TRUE --------------
 initUART('open')
 bufferPost = mp.Queue(600)
-verrou = mp.Lock()
+bufferIoT = mp.Queue(600)
 procPost = mp.Process(target=sendHttp, args=(bufferPost,))
+procIoTHub = mp.Process(target=iothub_client_telemetry_sample_run, args=(bufferIoT,))
 procPost.start()
+procIoTHub.start()
 
 while(1):
     try:
@@ -99,4 +138,5 @@ while(1):
     except KeyboardInterrupt:
         initUART('close')
         procPost.terminate()
+        procIoTHub.terminate()
         sys.exit()
